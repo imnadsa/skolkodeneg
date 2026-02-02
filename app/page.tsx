dcbe6d5cccbb945dbb8fc650ac34b088865a3337
imnadsa <antosh1109@mail.ru>
2026-02-03 03:34:21 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from '@/components/sections/Hero'
+import ForWho from '@/components/sections/ForWho'
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Hero />
+      <ForWho />
     </main>
   )
 }
