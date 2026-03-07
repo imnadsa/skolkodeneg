5def79b1e78b047c4c5335fb7890200044dc76de
imnadsa <antosh1109@mail.ru>
2026-03-07 22:03:02 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/sections/Hero'
 import ForWho from '@/components/sections/ForWho'
+import Inregration from '@/components/sections/Inregration'
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Hero />
       <ForWho />
+      <Inregration />
     </main>
   )
 }
