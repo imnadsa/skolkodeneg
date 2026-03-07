2f85e7845c4a3c3da53d321ef93d8a3efcc5d0b5
imnadsa <antosh1109@mail.ru>
2026-03-07 21:47:16 +0400

    Update fonts.ts

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,35 +1,19 @@
 import localFont from 'next/font/local'
 
-export const coolvetica = localFont({
-  src: '../public/fonts/coolvetica rg.ttf',
-  variable: '--font-coolvetica',
-  display: 'swap',
-})
-
 export const daysOne = localFont({
   src: '../public/fonts/DaysOne-Regular.ttf',
   variable: '--font-days-one',
   display: 'swap',
 })
 
-export const navigo = localFont({
-  src: [
-    {
-      path: '../public/fonts/Navigo-Regular.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/Navigo-Medium.ttf',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/Navigo-Bold.ttf',
-      weight: '700',
-      style: 'normal',
-    },
-  ],
-  variable: '--font-navigo',
+export const museoSans = localFont({
+  src: '../public/fonts/Museo Sans Cyrl 500.ttf',
+  variable: '--font-museo-sans',
+  display: 'swap',
+})
+
+export const sofiaSans = localFont({
+  src: '../public/fonts/SofiaSans-Regular.ttf',
+  variable: '--font-sofia-sans',
   display: 'swap',
 })
