470ffa7836aee419eb7170e5eab4ce0d722b1551
imnadsa <antosh1109@mail.ru>
2026-03-16 18:30:19 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from '@/components/sections/Hero'
 import ForWho from '@/components/sections/ForWho'
 import Integration from '@/components/sections/Integration'
 import FeaturesSlider from '@/components/sections/FeaturesSlider'
+import Pricing from '@/components/sections/Pricing'
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <ForWho />
       <Integration /> 
       <FeaturesSlider />
+      <Pricing />
     </main>
   )
 }
